f1a180a1942a9bfc0ea2179a639a8c434c1fcbda
Nick Barfoot <nick.barfoot@canvaspop.com>
2013-11-13 17:13:14 -0500

    Initial commit containing Pop-up Store framework and bundle files

---
+++ PopUpLib.framework/Versions/A/Headers/PopUpLib.h
@@ -0,0 +1,19 @@
+//
+//  PopUpLib.h
+//  PopUpLib
+//
+//  Created by Nick Barfoot @ CanvasPop
+//  Copyright (c) 2013 CanvasPop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <PopUpLib/PopupStoreController.h>
+
+@class PopUpLib;
+
+@interface PopUpLib : NSObject
+
+@end
+
+
---
+++ PopUpLib.framework/Versions/A/Headers/PopupStoreController.h
@@ -0,0 +1,32 @@
+//
+//  PopupStoreController.h
+//  PopUpLib
+//
+//  Created by Nick Barfoot @ CanvasPop on 11/1/2013.
+//  Copyright (c) 2013 CanvasPop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PopupStoreController;
+
+@protocol PopupStoreControllerDelegate <NSObject>
+    @required
+    - (void)popupStoreLibWasClosed:(PopupStoreController *)controller;
+    @optional
+    - (void)popupStoreLibWasOpened:(PopupStoreController *)controller;
+    - (void)popupStoreLibPrintHandshakeCompleted:(PopupStoreController *)controller;
+    - (void)popupStoreLibUserTappedContinue:(PopupStoreController *)controller;
+    - (void)popupStoreWasLoadedWithImage:(PopupStoreController *)controller;
+    - (void)popupStoreEventWasEmitted:(PopupStoreController *)controller withEvent:(NSString*)eventName withPayload:(NSDictionary *)payload;
+@end
+
+@interface PopupStoreController : UIViewController <UIWebViewDelegate>
+
+-(id)initWithImage:(UIImage *)image applicationKey:(NSString *)appKey delegate:(id <PopupStoreControllerDelegate>)aDelegate;
+
+@property (nonatomic, weak) id <PopupStoreControllerDelegate> storeDelegate;
+@property (nonatomic, weak) NSString *applicationKey;
+@property (nonatomic, strong) UIImage *storeImage;
+
+@end
